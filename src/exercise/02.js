b0f566582f2c6896fac7d12ec7663a6c339ffbbd
iamrosada <luisrosada2018@gmail.com>
2022-07-20 14:22:36 +0300

    using the lazy case

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,9 +6,10 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(
-    window.localStorage.getItem('name') || initialName,
-  )
+  function getInitialName() {
+    return window.localStorage.getItem('name') || initialName
+  }
+  const [name, setName] = React.useState(getInitialName)
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
